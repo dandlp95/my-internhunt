8ec4be2cceaa63b6c50e3de18a2e4e572ec0660d
Daniel De La Pena <del17011@byui.edu>
2022-12-09 23:27:09 -0700

    account portal error handling

--- src/pages/accountPortal.js
+++ src/pages/accountPortal.js
@@ -6,7 +6,6 @@ import FetchCalls from "../utils/fetchCalls";
 import CommentPreview from "../components/commentPreview";
 import { isAuth } from "../utils/isLoggedIn";
 import "./accountPortal.css";
-import { getApiRoot } from "../utils/getApiRoot";
 
 const defaultProfilePic = `iVBORw0KGgoAAAANSUhEUgAAAOEAAADhCAMAAAAJbSJIAAAASFBMVEWpqan39/f+/v6qqqr8/Pympqb29vajo6OxsbHu7u7z8/OgoKDKysqurq7k5OS9vb3Y2Njm5ube3t64uLjExMTR0dHNzc3a2toYtRwDAAAHKklEQVR4nO2daZPbIAyGjZGQ8X1m//8/LThJN3d8kCB2eKZNP2yn47cCXRYkSSKRSCQSiUQikUgkEolEIpFIJBKJRCKRSCQSiUQikUgkEon8bTBBRN8P8RGMLqKioD+nUJrfSIhZU5bTT//TTmUzSKLjT/4EiLJptYD0krwvB6Pb97M5AKkqO0hBWFlw/mU+zKfqBwp7vcqEsKnTV6i2opAXK475bKznmB/2WZBmlIlEOup7o9D8DasxOJWIRaZfrs9LmWIKb6lKmgBeL9ALhZDqgXw/8jqo6pYa8MwUVOjAUSw0368ZoZbhSKQpfeNCH2lMVYWBpDnYvvafT8kHlCFIpH69AU9mFEMAC7WyAjfpmzUGILFoU7FdIIiKt0SZYLnDgtbd5L41vEbisEugldixLhwxydcGwluFABPf7Mas0X6XvBMN262IOG4ME5cA5GwVJnLnEj1JTFuu67T4cWDCWWPmW8oTsq25zK3AtOZoRIlu3MwRnqlNtjMUXgAdQ4U2H3UGpBW/oC+lMwtaen47kUqXAlPhW889pJ3aMC3Z7cTKqT6TgHNr9+PkzpPOCG6uBld3D9/R+JZ0Ayq3JrT9U9+arhkc60tBF741XeM2VliUb0nX4I9rgZDyKjDcOxpgVurj4jdpyznwUri9R/qUlpXCynWsMPSsFGbuBc6NUz5EheErdFtZnBSySmo+obBmpTBx7kuBWSMDlWuF3OIhfSCn4dXHKJznpdy6wti6V1j5FnUFjs4FClYmdNrSP8Er4CcSlaM3a/+ZeNkQsXetcPCt6QbnG1HxMmFSYeW4BuZVHVro9cT6SmyXhptEbJwqzBN+CjF36WuYeVILkst3M1BJhqNf6NCGP7xqwxncPhp8BzDLSc+guzf5PUMTWmjfbOkZSMG3kmdg4eRdPsy1Lz8/k9j0O3Mym9ghstQ3H8ab9o+2CWB9lg27/VtxZH3qUu5tugHLUPiLRLMV9wnk1em+R0oad5wnSVPN04leYiTu2Iqa+YGSIzvK/bwinvnaNVg0sKHgBwDN2YleYDbSoNZP7UNa+37yxcgEK738FPBJnx3zCuL04QwitasUQqrCOupsTEFDvmaN9lV4R/KRJrHIjCGeVT8iSfaLFOYj8zzmGWapFlUv0vR5BmCvlNAjhnG++QmIh+6pREhF31AoR9SfgJIoOzx+Qwx9gxS0/f6DRMNUa/Hfs4i8a8uqCCSFWYQpq0xdK7OhaZphqJAKY7y/JHDeafONWMn5I+zNF4lEIpFIJBKJBMJfz7v/nr6jIrQXJONcKc4kpw5w6GWiUXEUVdn6dywt49gMWZXMhXDgbQx7v3XVlG2tjzclA4AQtuFv/hAq7/ppHOz912GZUdp1Zy1HlDVtJ45yHqDUrDbN62mQVmUwxrQSC8zKOk/Tx9quhdrWuOimJqEAOt9zLwapGvv8meUeSZwBUZfZcZSGrzGthyQ5drDEdveY/xQ9ZcR1XmiGcDQbT6lNAkVu7AiiOyQsL9yVWCWUTWrx2nxly7pBYrdUTchrOpFutN6tRNAlMnshhclBw3+nsY/5H4F8SohNumO8/JSDG/P9AspqZAFhmbvYfrcKc6EmDjdgI5VO3Ms9xiWDKE2E9KnS5GaN/oi8E8bnjF7v3Mes/oz9zlg7dpm/+EgHN97ztUj71Ql+ZlEo084d6BOR+vtmtAnotC373ARMX6+uMPuoh7mXqL854G6Husqv6rO7UX11sIi6L67QM/C9c6WYfSKHeYuGLw1JS3u+SW2tAfcBAyUfnpO2UzCtDwOeJZbFhyOjiRLdl4LgQxS0ptT+ZNxA+d0gcQ/0hB9cqJ58zDW6+FQuLq1A3/IsuvrQMpXYfCHRXoKWH3E3iIPwEyPuUNaK7ncjNiyW6BEt3ddTxoJM1uiMdu5taPCt6YbOsUTjRRkZcMb1nXw5pyV6pHVnRZOM+s5kHgEO76yjzn8m8whnlynTj4d6dwFKOPqiDyp5WlDZFNWFQByWv7D+OjXtHlYxuQM7J/pLDuP+5I2rlzmzt8mIhc+exRJ0se/lFDaMN6FFwbQrB5cVo3riMTlke3IbrH0LWIDers9+N3MIbL6+TiaS9x48YVKbrSakGvhVFPcoqLcOio2b57e+itqYgiPrZOYGvU1hH8QunNlWKnJrzLxACbXh6p5Ch2NCpTZ8TzKO4Qi0qJWnGiQS+3TtGmiLdS8z6JAGZsN8XSmMie8nXstcY6xR+M1pIDeofNUqleEE+xMm+xqXG1F6HUbYjF4RMPjXvQ9QK75sACffT7sJ6BdfXotBlBQPWChQJkx73G9ZmH/LJLR05pdlb00Rm+Bi4RlY9KYGsfP9oNs5LAoYWbAmXBgSqfX9mHtYMi2FwfoZAxzemxAbn8OVO1Gifh8vihD6+C94b0Pp+xH3AW87p1zf2S/mbR1MAQdDi3obLyrfj7gXdds3/QeNuX0CPDHbSQAAAABJRU5ErkJggg==`;
 
@@ -16,13 +15,12 @@ const AccountPortal = () => {
   const [posts, setPosts] = useState([]);
   const [comments, setComments] = useState([]);
   const [isAdmin, setIsAdmin] = useState(false);
-  const [isAccountAdmin, setIsAccountAdmin] = useState(false)
-  const [confirmationMessage, setBanConfirmationMessage] = useState();
+  const [isAccountAdmin, setIsAccountAdmin] = useState(false);
 
   const navigate = useNavigate();
 
   let userData = localStorage.getItem("userData");
-  userData = JSON.parse(userData);
+  userData = userData ? JSON.parse(userData) : null;
 
   var pronoun;
   var owner = false;
@@ -53,40 +51,52 @@ const AccountPortal = () => {
       const response = await isAuth();
       if (response.ok) {
         const visitorInfo = await response.json();
-        console.log("visitor info: ", visitorInfo);
         if (visitorInfo.accessLevel === 1) {
           setIsAdmin(true);
         }
+      } else if (response.err === "server down") {
+        alert("Servers down");
+        navigate("/");
+      } else {
+        alert("Please login");
+        navigate("/");
       }
     };
-    if (!owner) fetchVisitorInfo();
+    fetchVisitorInfo();
   }, []);
 
   useEffect(() => {
     const getUserInfo = async () => {
       var url;
-      if (isAdmin || owner) {
-        url = `/users/getByIdPrivate/${id}`;
-        const backendApi = new FetchCalls(url, "GET", userData.jwt);
-        const response = await backendApi.protectedNoBody();
-        if (response.ok) {
-          const usersResponse = await response.json();
-          setUser(usersResponse);
-          if (usersResponse.accessLevel === 1) setIsAccountAdmin(true);
+      try {
+        if (isAdmin || owner) {
+          url = `/users/getByIdPrivate/${id}`;
+          const backendApi = new FetchCalls(url, "GET", userData.jwt);
+          const response = await backendApi.protectedNoBody();
+          if (response.ok) {
+            const usersResponse = await response.json();
+            setUser(usersResponse);
+            if (usersResponse.accessLevel === 1) setIsAccountAdmin(true);
+          } else {
+            const serverError = await response.json();
+            alert(serverError.message);
+          }
         } else {
-          alert("error fetching user.");
+          url = `/users/getById/${id}`;
+          const backendApi = new FetchCalls(url, "GET");
+          const response = await backendApi.publicGet();
+          if (response.ok) {
+            const usersResponse = await response.json();
+            setUser(usersResponse);
+            if (usersResponse.accessLevel === 1) setIsAccountAdmin(true);
+          } else {
+            const serverError = await response.json();
+            alert(serverError.message);
+          }
         }
-      } else {
-        url = `/users/getById/${id}`;
-        const backendApi = new FetchCalls(url, "GET");
-        const response = await backendApi.publicGet();
-        if (response.ok) {
-          const usersResponse = await response.json();
-          setUser(usersResponse);
-          if (usersResponse.accessLevel === 1) setIsAccountAdmin(true);
-        } else {
-          alert("error fetching user.");
-        }
+      } catch (err) {
+
+
       }
     };
 
@@ -96,20 +106,33 @@ const AccountPortal = () => {
         "GET",
         userData.jwt
       );
-      const response = await backendApi.protectedNoBody();
-      if (response.ok) {
-        const comments = await response.json();
-        setComments(comments);
+      try {
+        const response = await backendApi.protectedNoBody();
+        if (response.ok) {
+          const comments = await response.json();
+          setComments(comments);
+        } else {
+          const backendError = await response.json();
+          alert(backendError.message);
+        }
+      } catch (err) {
+
       }
     };
 
     const getUserPosts = async () => {
       const backendApi = new FetchCalls(`/posts/getPostByUser/${id}`, "GET");
-      const response = await backendApi.publicGet();
-      console.log(response);
-      if (response.ok) {
-        const posts = await response.json();
-        setPosts(posts);
+      try {
+        const response = await backendApi.publicGet();
+        if (response.ok) {
+          const posts = await response.json();
+          setPosts(posts);
+        } else {
+          const backendErr = await response.json();
+          alert(backendErr.message);
+        }
+      } catch (err) {
+
       }
     };
 
@@ -174,7 +197,9 @@ const AccountPortal = () => {
                 </button>
               </div>
             )}
-            {isAccountAdmin &&<div className="admin-status">Moderator Account</div>}
+            {isAccountAdmin && (
+              <div className="admin-status">Moderator Account</div>
+            )}
             {isAdmin && !owner && (
               <div>
                 <div>
