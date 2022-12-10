fdafb9608dcfbadce5be7870d111bcfe16ba830a
Daniel De La Pena <del17011@byui.edu>
2022-12-09 23:29:39 -0700

    about page error handling

--- src/pages/aboutPage.js
+++ src/pages/aboutPage.js
@@ -6,27 +6,10 @@ import about from "../utils/about.json";
 import "./aboutPage.css#";
 
 const AboutPage = () => {
-  const [user, setUser] = useState([]);
-  const navigate = useNavigate();
-
-  useEffect(() => {
-    const isLoggedIn = async () => {
-      const res = await isAuth();
-      if (!res.ok) {
-        alert("Please log in");
-        navigate("/");
-      } else {
-        const userData = localStorage.getItem("userData");
-        const userDataJson = JSON.parse(userData);
-        setUser(userDataJson.userId);
-      }
-    };
-    isLoggedIn();
-  }, []);
 
   return (
     <div>
-      <Header accountId={user} />
+      <Header/>
       <div class="spacer">&nbsp;</div>
       <div className="about-page">
         <h2>{about.title}</h2>
